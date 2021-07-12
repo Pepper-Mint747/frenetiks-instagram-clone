4a29b3977fd8e174fbd9de74a56fc6b40e4ea22d
Brian <omogabrian2060@outlook.com>
2021-07-12 19:56:32 +0300

    fixed jwt

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports={
     MONGO_URI:process.env.MONGO_URI,
-    JWT_SECRET:process.env.JWT_SEC,
+    JWT_SECRET:process.env.JWT_SECRET,
     SENDGRID_API:process.env.SENDGRID_API,
     EMAIL:process.env.EMAIL
 }
